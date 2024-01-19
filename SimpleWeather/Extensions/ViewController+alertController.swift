85567fa6469d1e785780f56a1165f31a5f36e98f
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-19 19:14:43 +0300

    refactor vc extencion

--- SimpleWeather/Extensions/ViewController+alertController.swift
+++ SimpleWeather/Extensions/ViewController+alertController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 extension ViewController {
     func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
-        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
-        ac.addTextField { tf in
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        alertController.addTextField { textField in
             let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
-            tf.placeholder = cities.randomElement()
+            textField.placeholder = cities.randomElement()
         }
         let search = UIAlertAction(title: "Search", style: .default) { action in
-            let textField = ac.textFields?.first
+            let textField = alertController.textFields?.first
             guard let cityName = textField?.text else { return }
             if cityName != "" {
                 print("search info for the \(cityName)")
@@ -24,8 +24,8 @@ extension ViewController {
         }
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        ac.addAction(search)
-        ac.addAction(cancel)
-        present(ac, animated: true, completion: nil)
+        alertController.addAction(search)
+        alertController.addAction(cancel)
+        present(alertController, animated: true, completion: nil)
     }
 }
