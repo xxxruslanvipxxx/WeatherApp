9569cbc998d1cfaa64b7ac211de7eb1a7b66362e
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-22 21:24:54 +0300

    Complete NetworkWeatherManager

--- SimpleWeather/Models/NetworkWeatherManager.swift
+++ SimpleWeather/Models/NetworkWeatherManager.swift
@@ -9,22 +9,23 @@ struct NetworkWeatherManager {
         if let url = URL(string: urlString) {
             let dataTask = session.dataTask(with: url) { data, response, error in
                 if let data = data {
-                    self.parseJSON(with: data)
+                    let currentWeather = self.parseJSON(with: data)
                 }
             }
             dataTask.resume()
         }
     }
     
-    func parseJSON(with data: Data) {
+    func parseJSON(with data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-            print(currentWeatherData.current.temp)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else { return nil }
+            return currentWeather
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
+        return nil
     }
     
 }
