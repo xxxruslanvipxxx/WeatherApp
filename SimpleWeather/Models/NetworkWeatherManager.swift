b201eec19b8acc8f42832b543a797ce32030d35f
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-23 17:08:47 +0300

    Add completionHandler to NetworkWeatherManager.fetchCurrentWeather

--- SimpleWeather/Models/NetworkWeatherManager.swift
+++ SimpleWeather/Models/NetworkWeatherManager.swift
@@ -2,21 +2,23 @@ import Foundation
 
 struct NetworkWeatherManager {
     
-    func fetchCurrentWeather(for city: String) {
+    func fetchCurrentWeather(forCity city: String, completionHandler: @escaping (CurrentWeather) -> Void) {
         let urlString = "https://api.weatherapi.com/v1/current.json?q=\(city)&key=\(Constants.apiKey)"
         
         let session = URLSession(configuration: .default)
         if let url = URL(string: urlString) {
             let dataTask = session.dataTask(with: url) { data, response, error in
                 if let data = data {
-                    let currentWeather = self.parseJSON(with: data)
+                    if let currentWeather = self.parseJSON(withData: data) {
+                        completionHandler(currentWeather)
+                    }
                 }
             }
             dataTask.resume()
         }
     }
     
-    func parseJSON(with data: Data) -> CurrentWeather? {
+    func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
