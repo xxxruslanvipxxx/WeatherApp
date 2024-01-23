c9236c62b64eca4db956a9291ddb6a1a79c208f3
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-23 18:33:31 +0300

    Setup outlets

--- SimpleWeather/View Controllers/ViewController.swift
+++ SimpleWeather/View Controllers/ViewController.swift
@@ -11,13 +11,27 @@ class ViewController: UIViewController {
     
     @IBAction func searchPressed(_ sender: UIButton) {
         self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { cityName in
-            self.networkWeatherManager.fetchCurrentWeather(for: cityName)
+            self.networkWeatherManager.fetchCurrentWeather(forCity: cityName) { currentWeather in
+                DispatchQueue.main.async {
+                    self.cityLabel.text = currentWeather.cityName
+                    self.temperatureLabel.text = currentWeather.currentTemperatureString
+                    self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
+                }
+            }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-          
+    
+        self.networkWeatherManager.fetchCurrentWeather(forCity: cityLabel.text!) { currentWeather in
+            DispatchQueue.main.async {
+                self.cityLabel.text = currentWeather.cityName
+                self.temperatureLabel.text = currentWeather.currentTemperatureString
+                self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
+            }
+        }
+        
     }
 }
 
