100b8d0ebc6fb5ffef6dd054d63647df1b010501
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-25 16:11:25 +0300

    refactor (add onCompletion)

--- SimpleWeather/View Controllers/ViewController.swift
+++ SimpleWeather/View Controllers/ViewController.swift
@@ -7,32 +7,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
-    let networkWeatherManager = NetworkWeatherManager()
+    var networkWeatherManager = NetworkWeatherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { cityName in
-            self.networkWeatherManager.fetchCurrentWeather(forCity: cityName) { currentWeather in
-                DispatchQueue.main.async {
-                    self.cityLabel.text = currentWeather.cityName
-                    self.temperatureLabel.text = currentWeather.currentTemperatureString
-                    self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
-                }
-            }
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [weak self] cityName in
+            self?.networkWeatherManager.fetchCurrentWeather(forCity: cityName)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else {return}
+            self.updateInterface(with: currentWeather)
+        }
+        self.networkWeatherManager.fetchCurrentWeather(forCity: cityLabel.text!)
+    }
     
-        self.networkWeatherManager.fetchCurrentWeather(forCity: cityLabel.text!) { currentWeather in
-            DispatchQueue.main.async {
-                self.cityLabel.text = currentWeather.cityName
-                self.temperatureLabel.text = currentWeather.currentTemperatureString
-                self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
+    private func updateInterface(with weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.currentTemperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            
+            let imageString = weather.systemIconNameString
+            print(imageString)
+            if let image = UIImage(systemName: imageString) {
+                self.weatherIconImageView.image = image
             }
         }
-        
     }
 }
-
-
