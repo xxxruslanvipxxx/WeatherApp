3e8926523295ac84f17ffc89832d6f39fe3a352f
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-25 20:55:34 +0300

    Refactor NetworkWeatherManager (now you can get data from coord's)

--- SimpleWeather/Models/NetworkWeatherManager.swift
+++ SimpleWeather/Models/NetworkWeatherManager.swift
@@ -1,13 +1,27 @@
 import Foundation
+import CoreLocation
 
 class NetworkWeatherManager {
     
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
     var onCompletion: ((CurrentWeather) -> Void)?
     
-    func fetchCurrentWeather(forCity city: String) {
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.weatherapi.com/v1/current.json?q=\(city)&key=\(Constants.apiKey)"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.weatherapi.com/v1/current.json?q=\(latitude),\(longitude)&key=\(Constants.apiKey)"
+        }
         
-        let urlString = "https://api.weatherapi.com/v1/current.json?q=\(city)&key=\(Constants.apiKey)"
-        
+        performRequest(withURLString: urlString)
+    }
+    
+    fileprivate func performRequest(withURLString urlString: String) {
         let session = URLSession(configuration: .default)
         if let url = URL(string: urlString) {
             let dataTask = session.dataTask(with: url) { data, response, error in
@@ -21,11 +35,10 @@ class NetworkWeatherManager {
         }
     }
     
-    func parseJSON(withData data: Data) -> CurrentWeather? {
+    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-            print(currentWeatherData.current.condition.code)
             guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else { return nil }
             return currentWeather
         } catch let error as NSError {
