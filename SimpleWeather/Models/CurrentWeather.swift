a2978cba814da992379ce91ace550d55e8de2ae2
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-25 16:09:58 +0300

    Add systemIconNameString, temp string formatted

--- SimpleWeather/Models/CurrentWeather.swift
+++ SimpleWeather/Models/CurrentWeather.swift
@@ -6,17 +6,30 @@ struct CurrentWeather {
     
     let cityName: String
     
-    let currentTemperature: Double
+    private let currentTemperature: Double
     var currentTemperatureString: String {
-        return "\(currentTemperature.rounded())"
+        return String(format: "%.0f", currentTemperature.rounded())
     }
     
-    let feelsLikeTemperature: Double
+    private let feelsLikeTemperature: Double
     var feelsLikeTemperatureString: String {
-        return "\(feelsLikeTemperature.rounded())"
+        return String(format: "%.0f", feelsLikeTemperature.rounded())
     }
     
-    let conditionCode: Int
+    private let conditionCode: Int
+    var systemIconNameString: String {
+        switch conditionCode {
+        case 1000: return "sun.max.fill"
+        case 1003, 1009: return "cloud.fill"
+        case 1030, 1135, 1147: return "cloud.fog.fill"
+        case 1063, 1180, 1186: return "cloud.sun.rain.fill"
+        case 1150, 1153, 1171: return "cloud.drizzle.fill"
+        case 1183, 1189, 1192, 1195, 1198, 1201, 1072, 1240, 1243, 1246: return "cloud.rain.fill"
+        case 1066, 1069, 1204, 1114, 1117, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264, 1279, 1282 : return "cloud.snow.fill"
+        case 1087, 1273, 1276: return "cloud.bolt.rain.fill"
+        default: return "icloud.slash.fill"
+        }
+    }
     
     init?(currentWeatherData: CurrentWeatherData) {
         self.cityName = currentWeatherData.location.name
